82c2d99ca430a373a608622d9ac02b263812dfe2
Ennio Marke <mystical.ennio@gmail.com>
2025-02-04 21:04:24 +0100

    v2.0

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "public/index.html",
+    "public/js/**/*.js",
+    "public/assets/**/*.*",
+    "public/ui/**/*.*",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
